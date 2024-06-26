22a501993c252109cc340e58a9e899304a55fb8e
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-06-26 11:15:08 -0700

    Add files via upload

---
+++ build/_app/immutable/chunks/navigation.85b9e1ba.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.2d58dd5e.js";const e=o("goto");export{e as g};
---
+++ build/_app/immutable/nodes/13.90d6c07e.js
@@ -0,0 +1,14 @@
+import{s as v,n as o}from"../chunks/scheduler.05aee0d7.js";import{S as f,i as w,g as d,s as g,h,A as u,c as y,k as m,a as n,f as l}from"../chunks/index.c058c29e.js";const _=""+new URL("../assets/enzz.e1b240a6.jpg",import.meta.url).href;function x(p){let e,r="Services",a,s,c=`<img class="backBack svelte-18w91zq" src="${_}" alt="background"/> <p class="text_Box cut_Box overLay glass_Back svelte-18w91zq">Exciting developments are on the horizon! We are thrilled to announce that services like
+		appointments for photo and video shoots will soon be available. This addition will allow clients
+		to easily schedule and manage their sessions with our talented team. Whether you&#39;re looking for
+		professional headshots, creative portraits, or high-quality video production, our upcoming
+		booking system will streamline the process, making it simpler than ever to secure your desired
+		time slot. Stay tuned for more details on how to access and utilize this convenient service.
+
+		<br class="svelte-18w91zq"/><br class="svelte-18w91zq"/>
+		Our new appointment scheduling service will offer a seamless and user-friendly experience. Clients
+		will be able to view available dates and times, select their preferred slots, and receive immediate
+		confirmation. This system aims to enhance our service delivery by reducing wait times and ensuring
+		that each session is perfectly tailored to your needs. We are committed to providing exceptional
+		service and look forward to offering this new feature to enhance your experience with us. Keep an
+		eye out for the official launch and be ready to book your shoots with ease and confidence.</p>`;return{c(){e=d("h1"),e.textContent=r,a=g(),s=d("div"),s.innerHTML=c,this.h()},l(t){e=h(t,"H1",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-u6uijw"&&(e.textContent=r),a=y(t),s=h(t,"DIV",{class:!0,"data-svelte-h":!0}),u(s)!=="svelte-lc0pnr"&&(s.innerHTML=c),this.h()},h(){m(e,"class","svelte-18w91zq"),m(s,"class","text_Block svelte-18w91zq")},m(t,i){n(t,e,i),n(t,a,i),n(t,s,i)},p:o,i:o,o,d(t){t&&(l(e),l(a),l(s))}}}class z extends f{constructor(e){super(),w(this,e,null,x,v,{})}}export{z as component};
