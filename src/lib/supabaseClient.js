997d9251d2881d5384f04928db92df7bbcf3d2ac
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-07-01 10:32:11 -0700

    Delete src/lib/supabaseClient.js

--- src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://adgdwehuxdddcwmfhudx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZ2R3ZWh1eGRkZGN3bWZodWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2NDY5MjMsImV4cCI6MjAzNDIyMjkyM30.MpxDNSoRD3zp6FXPXLOStxpY2PG9llWDaVhrRrJU7_M'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
