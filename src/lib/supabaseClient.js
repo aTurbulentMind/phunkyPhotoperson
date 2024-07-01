d5ae1398a43ee560cd99a87bf16a16ebe11303e5
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-07-01 10:28:46 -0700

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://adgdwehuxdddcwmfhudx.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZ2R3ZWh1eGRkZGN3bWZodWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2NDY5MjMsImV4cCI6MjAzNDIyMjkyM30.MpxDNSoRD3zp6FXPXLOStxpY2PG9llWDaVhrRrJU7_M'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
